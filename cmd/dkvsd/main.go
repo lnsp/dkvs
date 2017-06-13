78c5cfe52160fdee1dca0cdb8520a48b87f4ba85
Lennart Espe <lennart@espe.tech>
2017-06-13 16:53:19 +0200

    add dkvsd command doc

--- cmd/dkvsd/main.go
+++ cmd/dkvsd/main.go
@@ -1,3 +1,22 @@
+// Command dkvsd starts a new cluster node.
+//
+// Usage
+//    -local string
+//          set the local address (default "localhost:5000")
+//    -remote string
+//          set the remote address (default "localhost:5000")
+//    -replicas int
+//          number of replicas in cluster (default 1)
+//    -role string
+//          set node role to either master, slave or client (default "master")
+//    -v    display version of dkvs
+//
+// Examples:
+//    # start a master on the local machine.
+//    dkvsd -role master -replicas 3 -remote ""
+//    # start a node to join the cluster
+//    dkvsd -role slave -remote localhost:5000 -local localhost:5001
+//
 package main
 
 import (
@@ -9,7 +28,7 @@ import (
 )
 
 const (
-	versionText = "version:\n\tdkvs-0.0.1-indev"
+	versionText = "version:\n\tdkvsd-0.0.2-indev"
 )
 
 func main() {
