2756376269d1f9d07717dae4d21c7a26a29ce572
Lennart Espe <lennart@espe.tech>
2017-06-13 21:07:45 +0200

    add godoc to local

--- lib/local/local.go
+++ lib/local/local.go
@@ -7,10 +7,12 @@ import (
 	"github.com/lnsp/dkvs/lib/local/replicas"
 )
 
+// Node is a local node that can listen for incoming connections.
 type Node interface {
 	Listen() error
 }
 
+// Slave is a local slave runtime.
 type Slave struct {
 	PublicAddress string
 	ReplicaSet    replicas.Set
@@ -20,6 +22,7 @@ type Slave struct {
 	NodeStatus    lib.Status
 }
 
+// Master is a local master runtime.
 type Master struct {
 	Slave
 	ReplicationFactor int
